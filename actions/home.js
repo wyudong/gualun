44ea83b412cb5c7e77fc47a9f114b6b47d917d3e
yudong <wangyuuudong@gmail.com>
2023-03-19 18:08:58 +0800

    adjust delay

--- actions/home.js
+++ actions/home.js
@@ -7,7 +7,7 @@ const { KEY_HOME, POS_HOME_CONFIRM, KEY_SIT } = config
 module.exports.home = async () => {
   console.log(`press ${KEY_HOME}`)
   robot.keyTap(KEY_HOME)
-  await utils.sleep(1500)
+  await utils.sleep(1000)
   robot.moveMouse(POS_HOME_CONFIRM.x, POS_HOME_CONFIRM.y)
   robot.mouseClick('left', false)
   await utils.sleep(3000)
